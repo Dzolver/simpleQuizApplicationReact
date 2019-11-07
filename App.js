aa0bd10549f4b0c41871f2b00b7c5bf32b90ea3f
dzolver <kaitoano@gmail.com>
2019-11-07 17:18:55 +0530

    quiz shuffled decoded answer list with onclick events

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
+
 
 function QuizCover(props) {
   return (
@@ -21,6 +19,7 @@ function QuizCover(props) {
     </li>
   );
 }
+
 // function createQuizGame(buttonData){
 //   alert('clicked');
 //   console.log("data button clicked");
@@ -76,8 +75,8 @@ class Quiz extends React.Component {
   constructor() {
     super();
     this.state = {
-      quiz_number: 9,
-      current_question:null,
+      quiz_number: null,
+      current_question: null,
       questions: [],
       question_counter: 0,
       startDate: [],
@@ -86,26 +85,85 @@ class Quiz extends React.Component {
       correct_answers: [],
       incorrect_answers: [],
       current_answer_status: [],
-      current_answer_list: null
+      current_answer_list: [],
+      myJson: []
     }
   }
   highest_option_count = 4;
   componentDidMount() {
     //STUCK HERE
-    const response = fetch('https://opentdb.com/api.php?amount=10&category=' + this.state.quiz_number);
-    const myJson = response.data.json();
-    this.setState({
-      quiz_number: this.props.quizNumber,
-      quiz_data: myJson.results
-    });
-    console.log('mount data',this.state.quiz_data);
+    if (this.state.quiz_number == null) {
+      this.setState({
+        quiz_number: this.props.quizNumber
+      })
+      fetch('https://opentdb.com/api.php?amount=10&category=' + this.props.quizNumber)
+        .then((result) => {
+          result.json()
+            .then(myJson => {
+              this.setState({
+                myJson: myJson.results
+              }, () => {
+                console.log(this.state.myJson);
+
+              });
+            })
+        });
+    }
   }
-  componentDidUpdate(){
-    this.setState({
-      current_question:this.quiz_data[this.question_counter].question
-    });
-    console.log('new question',this.current_question);
+
+  nextQuestion = () => {
+    let { question_counter } = this.state;
+    this.setState({ question_counter: question_counter += 1 },
+      () => console.log(this.state.question_counter))
   }
+  decodeHtml = (html) => {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+  }
+  displayAnswer = (id, answer) =>{
+    return(
+      <div style={{float:'left',width:'45%',padding:'10px', backgroundColor:'black', marginRight:'30px',marginBottom:'30px',borderStyle:'solid',borderWidth:'1px',borderColor:'lime', borderRadius:'10px'}} onClick={this.nextQuestion}>
+<li id={'li'+id} style={{margin:'10px',backgroundColor:'black'}}>{this.decodeHtml(answer)}</li>
+      </div>
+      
+    )
+  }
+  displayAnswerList = ()=>{
+    let list = [];
+    list = this.state.myJson[this.state.question_counter].incorrect_answers
+    list.push(this.state.myJson[this.state.question_counter].correct_answer)
+    list = this.shuffle(list); 
+    let answerList = []
+    for(let i=0;i<4;i++){
+      if(list[i]==null){
+        continue;
+      }else{
+        answerList.push(this.displayAnswer(i,list[i]));
+      }
+      
+    }
+    return answerList;
+  }
+  shuffle=(array)=> {
+    console.log('raw array', array)
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    console.log('shuffled array ' + array);
+    return array;
+}
   createInterface() {
     console.log("quiz ", this.props.quizNumber);
     return (
@@ -113,22 +171,19 @@ class Quiz extends React.Component {
         <div>
           <h1>
             <div id="quiz-title-in-game">
-              <h1>ANSWER THE QUESTIONS AS FAST AS YOU CAN!</h1>
+              <h1 id='quiz-heading'>ANSWER THE QUESTIONS AS FAST AS YOU CAN!</h1>
               <h2>
-                <div id="quiz-question">
-                  THIS IS THE QUESTION
+                <div id="quiz-question" defaultValue="loading question">
+                  {this.state.myJson.length > 0 && this.state.question_counter !==10 && this.decodeHtml(this.state.myJson[this.state.question_counter].question)}
                 </div>
               </h2>
               <br />
               <h3>
-                <div id="quiz-answer-list">
-                  THIS IS THE ANSWER LIST
+                <div id="quiz-answer-list" style={{listStyleType:'none'}}>
+                  {this.state.myJson.length > 0  && this.state.question_counter !==10 && this.displayAnswerList()}
+                  <br/>
                 </div>
               </h3>
-              <div id="answer-container">
-                <button id='answer_button' className='btn btn-primary'
-                  style={{ marginLeft: '50px' }}>NEXT QUESTION</button>
-              </div>
               <h3>
                 <div id='stats' style={{ color: 'blue', marginLeft: '50px', marginTop: '20px' }}>
                 </div>
